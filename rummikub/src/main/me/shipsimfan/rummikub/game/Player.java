8955e8537ac38241d3cc1dba5df3da959394d073
Lance Hart <shipsimfan@hotmail.com>
2021-09-26 22:00:38 -0400

    CODE FOR player creation

--- rummikub/src/main/me/shipsimfan/rummikub/game/Player.java
+++ rummikub/src/main/me/shipsimfan/rummikub/game/Player.java
@@ -1,10 +1,27 @@
 package me.shipsimfan.rummikub.game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
+	private List<Tile> hand;
+
 	public Player(Tile[] startingHand) {
+		hand = new ArrayList<>(Arrays.asList(startingHand));
+		hand.sort(new Tile.TileComparator());
 	}
 
 	public String getHand() {
-		return "";
+		String string = "";
+
+		for (int i = 0; i < hand.size(); i++) {
+			if (i != 0)
+				string += ",";
+
+			string += hand.get(i).toString();
+		}
+
+		return string;
 	}
 }
--- rummikub/src/main/me/shipsimfan/rummikub/game/Tile.java
+++ rummikub/src/main/me/shipsimfan/rummikub/game/Tile.java
@@ -1,5 +1,67 @@
 package me.shipsimfan.rummikub.game;
 
+import java.security.InvalidParameterException;
+import java.util.Comparator;
+
 public class Tile {
-	public Tile(String name) {}
+	enum Color {
+		RED, BLUE, GREEN, ORANGE;
+
+		@Override
+		public String toString() {
+			return switch (this) {
+			case RED -> "R";
+			case BLUE -> "B";
+			case GREEN -> "G";
+			case ORANGE -> "O";
+			};
+		}
+	}
+
+	private Color color;
+	private int number;
+
+	public Tile(String name) {
+		color = switch (name.charAt(0)) {
+		case 'R' -> Color.RED;
+		case 'B' -> Color.BLUE;
+		case 'G' -> Color.GREEN;
+		case 'O' -> Color.ORANGE;
+		default -> throw new InvalidParameterException("Invalid color code for tile name");
+		};
+
+		number = Character.getNumericValue(name.charAt(1));
+		if (number == 1 && name.length() > 2) {
+			number *= 10;
+			number += Character.getNumericValue(name.charAt(2));
+		}
+
+		if (number < 1 || number > 13)
+			throw new InvalidParameterException("Invalid number for tile name");
+	}
+
+	@Override
+	public String toString() {
+		return color.toString() + number;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+
+		if (!(o instanceof Tile t))
+			return false;
+
+		return color == t.color && number == t.number;
+	}
+
+	public static class TileComparator implements Comparator<Tile> {
+		public int compare(Tile t1, Tile t2) {
+			if (t1.color == t2.color)
+				return t1.number - t2.number;
+
+			return t1.color.compareTo(t2.color);
+		}
+	}
 }
